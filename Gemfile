ced282d51e405ae3b443ec77bfcf7ee89d8df0c6
jackiejohnston <jackiejohnston5@gmail.com>
2014-04-02 20:39:33 -0700

    Generated new rails app, generated Devise user model

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+gem 'rails', '3.2.13'
+gem 'pg'
+gem 'devise'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'pry'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+  gem 'factory_girl_rails'
+  gem 'faker'
+  gem 'capybara'
+  gem 'selenium-webdriver'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '1f9f5e8ada3ea58681688c5e64ae1014016763cb9af7612308c77faafb302b224a82819665c5a178837a00fb883a1c33da5e92138c75b08c947602610573bc72'
