033f68c59ceb09b841fb62ec3a7194914cec8da7
Apprentice <apprentice@dbc29.local>
2014-04-03 16:25:43 -0700

    Added controller and model for questions
    * Controller includes actions for index, show, add and create
    * Updated migration
    * Model includes validations and associations

--- db/migrate/20140403230646_create_questions.rb
+++ db/migrate/20140403230646_create_questions.rb
@@ -1,6 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.belongs_to :user, :required => true
       t.string :title, :required => true
       t.text :body, :required => true
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140403144535) do
+ActiveRecord::Schema.define(:version => 20140403230646) do
+
+  create_table "questions", :force => true do |t|
+    t.string "title"
+    t.text   "body"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
