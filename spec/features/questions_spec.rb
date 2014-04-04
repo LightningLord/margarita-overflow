ce42860db0c77d154e259406f4e5dae9129f6014
Germain Boue & Janet Yi <pair+boue.krnwonseungee@devbootcamp.com>
2014-04-03 21:54:00 -0700

    3rd feature, user can login if they already have an account

--- spec/features/questions_spec.rb
+++ spec/features/questions_spec.rb
@@ -1,9 +1,21 @@
 require 'spec_helper'
 
 describe "Questions", :js => true do
-  let!(:question) { create :question }
+  # let!(:question) { create :question }
   describe "User can sign in" do
-      it "by signing up"
+      it "by signing up" do
+        visit '/'
+        click_link('Sign up')
+        fill_in('Email', :with => 'test@test.com')
+        fill_in('Username', :with => 'test')
+        fill_in('Password', :with => 'testingtesting')
+        fill_in('Password', :with => 'testingtesting')
+        click_button 'Sign up'
+        page.should have_content('Logged in as test@test.com')
+    end
+  end
+
+    it "if this person already has an account" do
       visit '/'
       click_link('Sign up')
       fill_in('Email', :with => 'test@test.com')
@@ -11,9 +23,12 @@ describe "Questions", :js => true do
       fill_in('Password', :with => 'testingtesting')
       fill_in('Password', :with => 'testingtesting')
       click_button 'Sign up'
+      click_button 'Login'
+      fill_in('Email', :with => 'test@test.com')
+      fill_in('Password', :with => 'testingtesting')
       page.should have_content('Logged in as test@test.com')
     end
-  end
+
 
   describe "User can log out" do
     it "should not have access to answers" do
