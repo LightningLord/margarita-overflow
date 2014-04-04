a552c0fca312aa6802bf32636edc3f7ae582188e
Germain Boue & Janet Yi <pair+boue.krnwonseungee@devbootcamp.com>
2014-04-03 21:44:07 -0700

    2nd feature, user can log out

--- spec/features/questions_spec.rb
+++ spec/features/questions_spec.rb
@@ -16,7 +16,16 @@ describe "Questions", :js => true do
   end
 
   describe "User can log out" do
-
+    it "should not have access to answers" do
+      visit '/'
+      click_link('Sign up')
+      fill_in('Email', :with => 'test@test.com')
+      fill_in('Username', :with => 'test')
+      fill_in('Password', :with => 'testingtesting')
+      fill_in('Password', :with => 'testingtesting')
+      click_link('Logout')
+      page.should have_content('Signed out successfully.')
+    end
   end
 
 
