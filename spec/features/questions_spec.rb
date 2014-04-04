7cbf8ed835b7d0ea3444f54636dfe2e5eceb6002
Germain Boue & Janet Yi <pair+boue.krnwonseungee@devbootcamp.com>
2014-04-03 21:35:33 -0700

    First Feature test to test signing up

--- spec/features/questions_spec.rb
+++ spec/features/questions_spec.rb
@@ -1,3 +1,27 @@
 require 'spec_helper'
-describe "Tasks", :js => true do
+
+describe "Questions", :js => true do
   let!(:question) { create :question }
+  describe "User can sign in" do
+      it "by signing up"
+      visit '/'
+      click_link('Sign up')
+      fill_in('Email', :with => 'test@test.com')
+      fill_in('Username', :with => 'test')
+      fill_in('Password', :with => 'testingtesting')
+      fill_in('Password', :with => 'testingtesting')
+      click_button 'Sign up'
+      page.should have_content('Logged in as test@test.com')
+    end
+  end
+
+  describe "User can log out" do
+
+  end
+
+
+
+end
+
+
+
