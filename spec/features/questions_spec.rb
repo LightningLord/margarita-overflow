658c05c230de4b2fc2c3f8568759c262dfddc501
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-04 16:00:52 -0700

    Add two feature tests
    - User can sign up
    
    - User can sign in if they already have an account

--- spec/features/questions_spec.rb
+++ spec/features/questions_spec.rb
@@ -1,75 +1,63 @@
 require 'spec_helper'
 
-describe "Questions", :js => true do
+describe "Questions", :js => false do
   # let!(:question) { create :question }
   #There is no way to do capybara like a before but we can use before to use less of repetition I believe but in Rspec. Still think we would need to go thru all these actions which is fine.
+
+  # sign up ##########
+  # visit '/'
+  # click_link('Sign up')
+  # fill_in('Email', :with => 'test@test.com')
+  # fill_in('Username', :with => 'test')
+  # fill_in('Password', :with => 'testingtesting')
+  # fill_in('Password', :with => 'testingtesting')
+  # click_button 'Sign up'
+
+## log in ##
+     # click_on 'Login'
+     #  fill_in('Email', :with => 'test@test.com')
+     #  fill_in('Password', :with => 'testingtesting')
+     #  click_button 'Sign in'
+
+
   describe "User can sign in" do
-      it "by signing up" do
-        visit '/'
-        click_link('Sign up')
-        fill_in('Email', :with => 'test@test.com')
-        fill_in('Username', :with => 'test')
-        fill_in('Password', :with => 'testingtesting')
-        fill_in('Password', :with => 'testingtesting')
-        click_button 'Sign up'
-        page.should have_content('Logged in as test@test.com')
+    it "by signing up" do
+      visit root_path
+      click_on 'Sign up'
+      fill_in 'Email', :with => 'ned@stark.com'
+      fill_in 'Username', :with => 'nedstark'
+      fill_in 'Password', :with => 'winteriscoming'
+      fill_in 'Password confirmation', :with => 'winteriscoming'
+      click_button 'Sign up'
+      expect(page).to have_content('Welcome, nedstark')
     end
   end
 
+    let(:user) {FactoryGirl.create(:user)}
     it "if this person already has an account" do
-      visit '/'
-      click_link('Sign up')
-      fill_in('Email', :with => 'test@test.com')
-      fill_in('Username', :with => 'test')
-      fill_in('Password', :with => 'testingtesting')
-      fill_in('Password', :with => 'testingtesting')
-      click_button 'Sign up'
-      click_button 'Login'
-      fill_in('Email', :with => 'test@test.com')
-      fill_in('Password', :with => 'testingtesting')
-      page.should have_content('Logged in as test@test.com')
+      visit root_path
+      click_on 'Login'
+      fill_in 'Email', :with => user.email
+      fill_in 'Password', :with => user.password
+      click_button 'Sign in'
+      expect(page).to have_content("Welcome, #{user.username}")
     end
 
 
   describe "User can log out" do
     it "should not have access to answers" do
-      visit '/'
-      click_link('Sign up')
-      fill_in('Email', :with => 'test@test.com')
-      fill_in('Username', :with => 'test')
-      fill_in('Password', :with => 'testingtesting')
-      fill_in('Password', :with => 'testingtesting')
-      click_link('Logout')
-      page.should have_content('Signed out successfully.')
+
     end
   end
 
   describe "User can ask a question" do
     it "only if they are logged in" do
-      visit '/'
-      click_link('Sign up')
-      fill_in('Email', :with => 'test@test.com')
-      fill_in('Username', :with => 'test')
-      fill_in('Password', :with => 'testingtesting')
-      fill_in('Password', :with => 'testingtesting')
-      click_button 'Sign up'
-      click_button 'Login'
-      fill_in('Email', :with => 'test@test.com')
-      fill_in('Password', :with => 'testingtesting')
-      click_link('Ask a question')
-      fill_in('Title', :with => 'yolo')
-      fill_in('Body', :with => 'Lorizzle ipsum dolizzle my shizz , my shizz bow wow wow elit. Nullizzle sapizzle velizzle, im in the shizzle break it down, suscipit quizzle, gravida vizzle, arcu. Pellentesque egizzle tortizzle. Sed eros.')
-      click_button 'Create Question'
+
       #have to figure out a way to click on create question for further the test
     end
   end
 
     it "if they are not logged in it will just render back page" do
-      click_link('Ask a question')
-      fill_in('Title', :with => 'yolo')
-      fill_in('Body', :with => 'Lorizzle ipsum dolizzle my shizz , my shizz bow wow wow elit. Nullizzle sapizzle velizzle, im in the shizzle break it down, suscipit quizzle, gravida vizzle, arcu. Pellentesque egizzle tortizzle. Sed eros.')
-      click_link('Create Question')
-      page.should have_content('Ask a question')
     end
 
 
