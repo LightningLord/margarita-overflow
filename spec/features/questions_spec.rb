38a4fa5cd00b3f51cfc61058ceddd7ed9841407d
Germain Boue & Janet Yi <pair+boue.krnwonseungee@devbootcamp.com>
2014-04-03 22:05:39 -0700

    Two new features
      *Testing user can ask a question
      * 2 scenarios
      * Only if they are logged in
      * If not logged in it should just render new page

--- spec/features/questions_spec.rb
+++ spec/features/questions_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "Questions", :js => true do
   # let!(:question) { create :question }
+  #There is no way to do capybara like a before but we can use before to use less of repetition I believe but in Rspec. Still think we would need to go thru all these actions which is fine.
   describe "User can sign in" do
       it "by signing up" do
         visit '/'
@@ -43,6 +44,33 @@ describe "Questions", :js => true do
     end
   end
 
+  describe "User can ask a question" do
+    it "only if they are logged in" do
+      visit '/'
+      click_link('Sign up')
+      fill_in('Email', :with => 'test@test.com')
+      fill_in('Username', :with => 'test')
+      fill_in('Password', :with => 'testingtesting')
+      fill_in('Password', :with => 'testingtesting')
+      click_button 'Sign up'
+      click_button 'Login'
+      fill_in('Email', :with => 'test@test.com')
+      fill_in('Password', :with => 'testingtesting')
+      click_link('Ask a question')
+      fill_in('Title', :with => 'yolo')
+      fill_in('Body', :with => 'Lorizzle ipsum dolizzle my shizz , my shizz bow wow wow elit. Nullizzle sapizzle velizzle, im in the shizzle break it down, suscipit quizzle, gravida vizzle, arcu. Pellentesque egizzle tortizzle. Sed eros.')
+      click_button 'Create Question'
+      #have to figure out a way to click on create question for further the test
+    end
+  end
+
+    it "if they are not logged in it will just render back page" do
+      click_link('Ask a question')
+      fill_in('Title', :with => 'yolo')
+      fill_in('Body', :with => 'Lorizzle ipsum dolizzle my shizz , my shizz bow wow wow elit. Nullizzle sapizzle velizzle, im in the shizzle break it down, suscipit quizzle, gravida vizzle, arcu. Pellentesque egizzle tortizzle. Sed eros.')
+      click_link('Create Question')
+      page.should have_content('Ask a question')
+    end
 
 
 end
