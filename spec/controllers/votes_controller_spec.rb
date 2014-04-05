1c318b5ddc27462660e9b4434d59e4ef95f87164
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-05 15:51:37 -0700

    votescontroller#create responds to json by rendering json tests passing

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -3,6 +3,7 @@ describe VotesController do
   let(:question){FactoryGirl.create(:question)}
   let(:user){FactoryGirl.create(:user)}
   let(:answer){FactoryGirl.create(:answer)}
+
   context "create" do
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
@@ -43,6 +44,19 @@ describe VotesController do
         :votable_type=> "Answer", :user_id=> user.id}
       }.to change{answer.reload.vote_count}.by(1)
     end
+
+    it "responds to json by rendering json for a question" do
+      post(:create, :vote_data => {:value => 1, :votable_id => question.id,
+      :votable_type=> "Question", :user_id=> user.id}, :format => :json)
+      expect(response.body).to eq(question.reload.to_json)
+    end
+
+    it "responds to json by rendering json for an answer" do
+      post(:create, :vote_data=> {:value => 1, :votable_id => answer.id,
+      :votable_type=> "Answer", :user_id=> user.id}, :format => :json)
+      expect(response.body).to eq(answer.reload.to_json)
+    end
+
 
   end
 end
