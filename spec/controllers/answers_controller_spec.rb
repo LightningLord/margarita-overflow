1af94adab7d00309c06a206df933b87ac4e60173
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-05 14:10:39 -0700

    Tested Answers#create
      *set up devise helpers in testing environment
      *answerscontroller#create test passing

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,13 +1,27 @@
 require 'spec_helper'
 
 describe AnswersController do
+  let (:answer){FactoryGirl.create(:answer)}
+  let(:user){FactoryGirl.create(:user)}
+  let(:question){FactoryGirl.create(:question)}
   context "update" do
-    let (:answer){FactoryGirl.create(:answer)}
     it "updates an answer page" do
       expect {
         put :update, :id => answer.id,
         content: "Winter is coming"
       }.to change{answer.reload.content}.to "Winter is coming"
+    end
+  end
+  context "create" do
+
+    it "creates a new user" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in user
+      expect {
+        post :create, :answer => (FactoryGirl.attributes_for(:answer).merge(
+        {:question_id => question.id}))
+      }.to change {Answer.count}.by(1)
+
     end
   end
 
