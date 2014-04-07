87ef892ea09c1524132e38abf7221f6fa39178f1
Strand McCutchen <strand@bettermistak.es>
2014-04-07 15:59:03 -0700

    Refactor: Replace type-checking with votable in Votes#create

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,16 +9,11 @@ class VotesController < ApplicationController
   def create
     vote = Vote.new(params[:vote_data])
     if current_user.voted?(vote.votable_type, vote.votable_id).empty?
-      if vote.save && vote.votable_type == "Question"
-        @question = Question.find(vote.votable_id)
-        @question.update_vote_count(vote.value)
+      if vote.save
+        @votable = vote.votable_type.constantize.find vote.votable_id
+        @votable.update_vote_count(vote.value)
         current_user.votes << vote
-        render json: @question.to_json
-      elsif vote.save && vote.votable_type == "Answer"
-        @answer = Answer.find(vote.votable_id)
-        @answer.update_vote_count(vote.value)
-        current_user.votes << vote
-        render json: @answer.to_json
+        render json: @votable.to_json
       end
     else
       render text: "You've already voted."
